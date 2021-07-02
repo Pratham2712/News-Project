699376191c1056bd97b28b824dc4a613e7ab3a41
Pratham2712 <prathamvaishya123@gmail.com>
2021-07-02 16:23:14 +0530

    first commit

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
-/* const key = '6d8d1aea6c0c4cb6a19666e4dcce37ae'; */
-const key = `${process.env.REACT_APP_API_KEY}`;
+const key = '6d8d1aea6c0c4cb6a19666e4dcce37ae';
+/* const key = 'e731c427c0544fb38ec9642e70dadd15'; */
 
 export const universalUrl = (query) => ` https://newsapi.org/v2/everything?q=${query}&apiKey=${key}`;
 
